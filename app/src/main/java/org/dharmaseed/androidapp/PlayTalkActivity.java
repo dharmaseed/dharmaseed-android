269e9acdba8853ddd6070c0f9537bb6f9dee5457
Brett Bethke <bbethke@airware.com>
2016-02-13 17:56:27 -0800

    Display talk duration

--- app/src/main/java/org/dharmaseed/androidapp/PlayTalkActivity.java
+++ app/src/main/java/org/dharmaseed/androidapp/PlayTalkActivity.java
@@ -32,10 +32,11 @@ public class PlayTalkActivity extends AppCompatActivity
         DBManager dbManager = new DBManager(this);
         SQLiteDatabase db = dbManager.getReadableDatabase();
         String query = String.format(
-                "SELECT %s, %s, %s, %s FROM %s, %s WHERE %s.%s=%s.%s AND %s.%s=%s",
+                "SELECT %s, %s, %s, %s, %s FROM %s, %s WHERE %s.%s=%s.%s AND %s.%s=%s",
                 DBManager.C.Talk.TITLE,
                 DBManager.C.Talk.DESCRIPTION,
                 DBManager.C.Talk.AUDIO_URL,
+                DBManager.C.Talk.DURATION_IN_MINUTES,
                 DBManager.C.Teacher.NAME,
                 DBManager.C.Talk.TABLE_NAME,
                 DBManager.C.Teacher.TABLE_NAME,
@@ -50,16 +51,30 @@ public class PlayTalkActivity extends AppCompatActivity
         Cursor cursor = db.rawQuery(query, null);
         if(cursor.moveToFirst()) {
             // Set the talk title
-            TextView title = (TextView) findViewById(R.id.play_talk_talk_title);
-            title.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBManager.C.Talk.TITLE)));
+            TextView titleView = (TextView) findViewById(R.id.play_talk_talk_title);
+            titleView.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBManager.C.Talk.TITLE)));
 
             // Set the teacher name
-            TextView teacher = (TextView) findViewById(R.id.play_talk_teacher);
-            teacher.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBManager.C.Teacher.NAME)));
+            TextView teacherView = (TextView) findViewById(R.id.play_talk_teacher);
+            teacherView.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBManager.C.Teacher.NAME)));
 
             // Set the talk description
-            TextView description = (TextView) findViewById(R.id.play_talk_talk_description);
-            description.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBManager.C.Talk.DESCRIPTION)));
+            TextView descriptionView = (TextView) findViewById(R.id.play_talk_talk_description);
+            descriptionView.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBManager.C.Talk.DESCRIPTION)));
+
+            // Set the talk duration
+            TextView durationView = (TextView) findViewById(R.id.play_talk_talk_duration);
+            double duration = cursor.getDouble(cursor.getColumnIndexOrThrow(DBManager.C.Talk.DURATION_IN_MINUTES));
+            int durationHr  = (int)Math.floor(duration/60);
+            int durationMin = (int)Math.floor(duration-durationHr*60);
+            int durationSec = (int)Math.floor((duration-Math.floor(duration))*60);
+            String durationStr;
+            if(durationHr > 0) {
+                durationStr = String.format("%02d:%02d:%02d", durationHr, durationMin, durationSec);
+            } else {
+                durationStr = String.format("%02d:%02d", durationMin, durationSec);
+            }
+            durationView.setText(durationStr);
 
             // Save the URL
             url = "http://www.dharmaseed.org" + cursor.getString(cursor.getColumnIndexOrThrow(DBManager.C.Talk.AUDIO_URL));
