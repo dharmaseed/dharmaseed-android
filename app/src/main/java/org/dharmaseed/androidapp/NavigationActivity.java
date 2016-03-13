d43bd3339f581b651b283ec50be775a19c8384dd
Brett Bethke <bbethke@airware.com>
2016-03-13 14:20:29 -0700

    Layout tweaks

--- app/src/main/java/org/dharmaseed/androidapp/NavigationActivity.java
+++ app/src/main/java/org/dharmaseed/androidapp/NavigationActivity.java
@@ -78,15 +78,6 @@ public class NavigationActivity extends AppCompatActivity
         new CenterFetcherTask(dbManager, this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         new TalkFetcherTask(dbManager, this).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
